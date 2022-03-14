c474d3d181c45d1891bce26234449117afbba2e4
Samdav1 <samdavmech@gmail.com>
2022-03-13 17:43:11 -0700

    God is Good

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints a phrase 
+ * 
+ * Return: 0 at the end of the program
+ *
+ */
+int main(void)
+{
+
+    puts("\"programming is like building a multilinqual puzzle");
+    return (0);
+
+}
+
+      
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,3 @@
+# This is a read me files
+
+This directory contains thr c programming code 
